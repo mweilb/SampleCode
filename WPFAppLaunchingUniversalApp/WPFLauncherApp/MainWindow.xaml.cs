4841c04b7294257a3ee293073c47b06386eefd4d
Michael Weilbacher <mweilb@microsoft.com>
2016-01-28 16:34:04 -0800

    Adding some status reporting and all seems good

--- WPFAppLaunchingUniversalApp/WPFLauncherApp/MainWindow.xaml.cs
+++ WPFAppLaunchingUniversalApp/WPFLauncherApp/MainWindow.xaml.cs
@@ -28,23 +28,30 @@ namespace WPFLauncherApp
     {
         readonly Uri uri = new Uri("minecraft://DoSomething?With=This");
         const string TargetPackageFamilyName = "Microsoft.MinecraftUWP_8wekyb3d8bbwe";
-
+        bool bLoading = false;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+ 
         protected override async void OnActivated(EventArgs e)
         {
-            //Test whether the app we want to launch is installed
-            var supportStatus = await Launcher.QueryUriSupportAsync(uri, LaunchQuerySupportType.Uri, TargetPackageFamilyName);
-            if (supportStatus != LaunchQuerySupportStatus.Available)
+            if (bLoading == false)
             {
-                //Uri uriNew = new Uri("ms-windows-store://pdp/?productid=9NBLGGH2JHXJ");
+                //Test whether the app we want to launch is installed
+                var supportStatus = await Launcher.QueryUriSupportAsync(uri, LaunchQuerySupportType.Uri, TargetPackageFamilyName);
+                if (supportStatus != LaunchQuerySupportStatus.Available)
+                {
+                    bLoading = true;
+                    Status.Text = "We need to install :) " + supportStatus.ToString();
 
-                AppInstallManager manager = new AppInstallManager();
-                var installItems = manager.StartAppInstallAsync("9NBLGGH2JHXJ", "", true, false);
-                Status.Text = "Because the app we need is " + supportStatus.ToString();
+                    AppInstallManager manager = new AppInstallManager();
+                    var installItems = await manager.StartAppInstallAsync("9NBLGGH2JHXJ", "0010", true, false);
+
+
+                    installItems.StatusChanged += new TypedEventHandler<AppInstallItem, System.Object>((app, obj) => StatusChangedUpdate(Status, app, obj));
+                    installItems.Completed += new TypedEventHandler<AppInstallItem, System.Object>((app, obj) => CompletedUpdate(Status, app, obj));
+                }
             }
         }
 
@@ -54,9 +61,24 @@ namespace WPFLauncherApp
             bool success = await Launcher.LaunchUriAsync(uri, options);
             Debug.WriteLine(success);
         }
- 
 
- 
+        private void StatusChangedUpdate(TextBlock statusText, AppInstallItem app, System.Object status)
+        {
+            Dispatcher.Invoke((Action)(() =>
+            {
+                var statusResult = app.GetCurrentStatus();
+                statusText.Text = "We are installing " + statusResult.BytesDownloaded + " of "
+                                                       + statusResult.DownloadSizeInBytes;
+            }));
+    
+        }
 
-}
+        private void CompletedUpdate(TextBlock statusText, AppInstallItem app, System.Object status)
+        {
+            Dispatcher.Invoke((Action)(() =>
+            {
+                statusText.Text = "Lets go";
+            }));
+        }
+    }
 }
